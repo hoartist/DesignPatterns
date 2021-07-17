595677ff333881c42fa45d370b74b15c57659977
hoartist <69453448+hoartist@users.noreply.github.com>
2021-07-17 23:40:43 +0900

    Add files via upload

---
+++ src/Component.java
@@ -0,0 +1,19 @@
+
+//Component 클래스
+
+public class Component {
+
+    public void operation(){
+        throw new UnsupportedOperationException();
+    }
+    public void add(Component component){
+        throw new UnsupportedOperationException();
+    }
+    public void remove(Component component){
+        throw new UnsupportedOperationException();
+    }
+    public Component getChild(int i){
+        throw new UnsupportedOperationException();
+    }
+
+}
